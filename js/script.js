d1fcf85e4add1b89369b52b7e2d665ac3b3bf09b
joarchaga <ingjosephchacon@gmail.com>
2023-05-09 23:09:11 -0300

    Agregando Boton de incrementar o decrementar plato

---
+++ js/script.js
@@ -0,0 +1,14 @@
+document.querySelector("#BtnRes1").addEventListener("click",ResUno);
+document.querySelector("#BtnSum1").addEventListener("click",SumUno);
+let CuentaResta=0;
+
+function SumUno() {
+   CuentaResta=CuentaResta+1;
+   document.getElementById("MsgCuentaResta").textContent = CuentaResta;
+}
+
+function ResUno() {
+    if (CuentaResta>0)
+    CuentaResta=CuentaResta-1;
+    document.getElementById("MsgCuentaResta").textContent = CuentaResta;
+}
